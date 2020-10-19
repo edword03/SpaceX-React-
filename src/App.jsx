698c58e16efae5687310d9b1a77b5e56d1a93936
edword03 <veretennikov_dmitriy01@mail.ru>
2020-10-19 21:00:28 +0400

    feat: finaly spacex app: added functional of all components

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import './style.css';
+
+import FetchData from './components/service/FetchData';
+
+import Header from './components/Header/Header';
+import Features from './components/Features/Features.jsx';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer.jsx';
+import Calendar from './components/Calendar/Calendar';
+import Details from './components/Details/Details';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+class App extends React.Component{
+  
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+    company: null,
+  };
+
+  componentDidMount() {
+    this.updateRocket();
+    this.updateCompany();
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) })
+        return data;
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+
+  updateCompany() {
+    this.fetchData.getCompany()
+      .then(company => this.setState({ company }));
+  }
+
+  render() {
+    return(
+      <BrowserRouter>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/>
+
+        <Route exact 
+          path='/' 
+          render={() => this.state.company &&
+            <Home company={this.state.company} />} />
+
+        <Route path='/rocket' 
+          render={() =>  this.state.rocketFeatures &&
+            <Features {...this.state.rocketFeatures} />} />
+  
+        <Route path='/calendar' component={Calendar} />
+        <Route path='/details/:id' component={Details} />
+
+        {this.state.company && <Footer {...this.state.company} />}
+      </BrowserRouter>
+      
+    )
+  }
+}
+
+export default App;
